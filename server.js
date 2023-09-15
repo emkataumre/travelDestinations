036f81fd0219a79c3baf37880da8e7d78d562a7f
Tsukani <34612305+Tsukani@users.noreply.github.com>
2023-09-15 14:27:31 +0200

    Cleaned up a bit

--- server.js
+++ server.js
@@ -19,52 +19,39 @@ const client = new MongoClient(uri, {
 });
 
 //Connect the server to the database
-async function connectToDB(destinationData, res) {
+
+async function insertIntoDB(destinationData) {
   try {
     await client.connect();
 
-    const myDB = client.db("travelDestinations");
-    const myColl = myDB.collection("destinations");
+    const collection = client.db("travelDestinations").collection("destinations");
+
     //Establish a connection with the database and create some basic consts to reference docs and collections
-    const result = await addDataToDataBase(destinationData, myColl); //destination data is the parsed body from line 64
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-
-    res.writeHead(201, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ destination: result.insertedId }));
+    await collection.insertOne(destinationData);
+    return {
+      success: true,
+      message: "Data inserted successfully"
+    }
   } catch (error) {
     if (error.code === 11000) {
       // Handle duplicate key error
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "Data already exists." }));
+      return {
+        success: false,
+        message: "Data already exists."
+      }
     } else {
       console.error(error);
-      res.writeHead(500, { "Content-Type": "text-plain" });
-      res.end("Internal Server Error");
+      return {
+        success: false,
+        message: "Internal Server Error"
+      }
     }
   } finally {
     await client.close();
   }
 }
 
-//Inserts one doc in the database
-async function addDataToDataBase(destinationData, myColl) {
-  //once again the parsed body
-  const result = await myColl.insertOne(destinationData);
-  return result;
-}
-
-// Connect to database
-//       connectToDB(parsedBody, res).catch((error) => {
-//         //Here we call the ConnectToDB function if we wanna post something. and we set up an error catcher
-//         console.error(error);
-//       });
-
 //Express server setup
-
 server.get("/", (req, res) => {
   res.sendFile("pages/index.html", { root: __dirname });
 });
@@ -73,8 +60,9 @@ server.get("/form", (req, res) => {
   res.sendFile("pages/form.html", { root: __dirname });
 });
 
-server.post("/api/addDestination", (req, res) => {
-  res.json({ message: req.body });
+server.post("/api/addDestination", async (req, res) => {
+  const response = await insertIntoDB(req.body);
+  res.json(response);
 });
 
 server.listen(3000, () => {
