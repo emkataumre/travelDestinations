685e2579db048eb3b96626f75cb584a7bbd61859
emkataumre <emo.vladinov@gmail.com>
2023-10-09 17:23:59 +0200

    Added some validation to the Destination schema, can possibly be extended but i ran out of ideas; Added notes.txt to write random thougts down

--- schemas/destination.js
+++ schemas/destination.js
@@ -1,13 +1,36 @@
 import mongoose from "mongoose";
+import { countries } from "../utils.js";
 
 const { Schema } = mongoose;
 
 const Destination = new Schema({
-  country: String,
-  title: String,
-  link: String,
-  arrivalDate: Date,
-  departureDate: Date,
+  country: {
+    type: String,
+    required: true,
+    enum: countries,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  arrivalDate: {
+    type: Date,
+    required: true,
+    validate: {
+      validator: (value) => {
+        return value < this.departureDate;
+      },
+      message: "Arrival date must be before departure date",
+    },
+  },
+  departureDate: {
+    type: Date,
+    required: true,
+  },
   image: String,
   description: String,
 });
