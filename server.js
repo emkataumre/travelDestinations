d5591c9c4ea4bfa684de7c8e05d401f48f306d5c
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2023-10-09 17:58:56 +0200

    adjust get request based on mongoose

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 import mongoose from "mongoose";
 import * as url from "url";
+import Destination from "./schemas/destination.js";
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const server = express();
 server.use(express.urlencoded({ extended: true }));
@@ -13,7 +14,6 @@ server.use(express.static("public"));
 server.use(cors());
 
 mongoose.connect("mongodb://127.0.0.1:27017/travelDestination");
-
 server.get("/", async (req, res) => {
   //__dirname means look from directory root
   res.sendFile("pages/index.html", { root: __dirname });
@@ -24,17 +24,23 @@ server.get("/form", (req, res) => {
 });
 //should be addDestinations in order to follow REST standarts
 server.post("/api/addDestination", async (req, res) => {
-  const destination = req.body;
-  const result = await destinationCollection.insertOne(destination);
-  console.log(result.insertedId.toString());
-  res.status(201).json({ message: destination });
+  // const destination = req.body;
+  // const result = await destinationCollection.insertOne(destination);
+  // console.log(result.insertedId.toString());
+  // res.status(201).json({ message: destination });
 });
 
 server.get("/api/getDestinations", async (req, res) => {
-  const result = await destinationCollection.find().toArray();
-  res.status(200).json(result);
+  await Destination.find()
+    .then((destinations) => {
+      console.log(destinations);
+      res.status(200).json(destinations);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
 });
-
 server.listen(3000, () => {
   console.log(`Listening on port 3000`);
 });
